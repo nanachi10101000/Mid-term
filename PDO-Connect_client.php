6f45e39f0b194fad5a060dae9ab8272bfb62d4ed
Ian Hsu <ianhsu0116@gmail.com>
2021-09-05 00:04:54 +0800

    initial commit

---
+++ PDO-Connect_client.php
@@ -0,0 +1,20 @@
+<?php
+//開啟session
+session_start();
+
+// PDO connect 是連接MySQL時更安全且業界在用的方法
+$servername = "localhost";
+$username = "admin";
+$password = "00000000";
+$dbname = "client";
+
+try {
+    $db_host = new PDO(
+      "mysql:host = {$servername}; dbname = {$dbname}; charset = utf8",
+      $username, $password
+    );
+//    echo "DB connected Successfully";
+} catch (PDOException $e) {
+    echo "BD connected fail <br />";
+    echo "Error: " . $e -> getMessage();
+};
